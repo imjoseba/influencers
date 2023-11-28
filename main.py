bb3cfa8b161a7bab103cd3d357c4750872a07556
imjoseba <jmorenoiriarte@gmail.com>
2023-11-28 17:47:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ tasa_interaccion_promedio = df3['Tasa_interacción_60d'].mean()
 col1, col2, col3 = st.columns(3)
 blue_light_color = "#2f9cb3"
 with col1:
-    st.markdown('<p class="big-font">Suma de Seguidores del top 200</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font"> Suma de Seguidores del top 200</p>', unsafe_allow_html=True)
     st.markdown(f'<h1 style="color:{blue_light_color};">{total_seguidores:,}</h1>', unsafe_allow_html=True)
 
 with col2:
